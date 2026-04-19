eebdf78068ecc00de971660c2aba5f35752f9f90
lekhalipi2-star <lekhalipi2@gmail.com>
2026-04-19 13:06:46 +0530

    Update storyRoutes.js

--- backend/routes/storyRoutes.js
+++ backend/routes/storyRoutes.js
@@ -9,7 +9,7 @@ router.get("/", getStories);
 router.post("/", verifyToken, upload.single("cover"), addStory);
 router.put("/:id", verifyToken, updateStory);
 router.delete("/:id", verifyToken, deleteStory);
-router.post("/feedback", verifyToken, addFeedback);
+router.post("/feedback", addFeedback);
 router.get("/feedback", verifyToken, getFeedback);
 router.get("/:id", getStoryById);
 
