9e66fd64913d4583675d8f14399651e34004770a
lekhalipi2-star <lekhalipi2@gmail.com>
2026-04-23 12:52:57 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { login, verifySession } from "../controllers/authController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-router.post("/create-admin", createAdmin); // use once
+
 router.post("/login", login);
 router.get("/verify", verifyToken, verifySession);
 
