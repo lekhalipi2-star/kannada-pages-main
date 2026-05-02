cac63b9bb030615341e52dcad6b63e0e4bd7221d
lekhalipi2-star <lekhalipi2@gmail.com>
2026-05-02 22:43:00 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://api.lekhalipi.com';
 const BASE_ORIGIN = BASE_URL;
 export default BASE_URL;
 
