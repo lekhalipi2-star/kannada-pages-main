7bdc2b90b565ba962bb109253de0c32cb4f07afb
lekhalipi2-star <lekhalipi2@gmail.com>
2026-05-03 12:39:29 +0530

    Update storyRoutes.js

--- backend/routes/storyRoutes.js
+++ backend/routes/storyRoutes.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { getStories, getStoryById, addStory, updateStory, deleteStory, addFeedback, getFeedback } from "../controllers/storyController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
-import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.get("/", getStories);
-router.post("/", verifyToken, upload.single("cover"), addStory);
+router.post("/", verifyToken, addStory);
 router.put("/:id", verifyToken, updateStory);
 router.delete("/:id", verifyToken, deleteStory);
 router.post("/feedback", addFeedback);
